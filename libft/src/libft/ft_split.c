b41148551e43d261382dce2ff56105cfbbcf5221
habvi <a2231250601h@gmail.com>
2023-02-20 20:13:35 +0100

    update: ft_split

--- libft/src/libft/ft_split.c
+++ libft/src/libft/ft_split.c
@@ -1,57 +1,59 @@
 #include "libft.h"
 
-static size_t	count_words(char const *lp, char c)
+static size_t	count_words(char const *head, char c)
 {
-	char const	*rp = lp;
+	char const	*left = head;
+	char const	*right = head;
 	size_t		words;
 
 	words = 0;
-	while (*rp != '\0')
+	while (*right != '\0')
 	{
-		while (*rp == c)
+		while (*right == c)
 		{
-			lp++;
-			rp++;
+			left++;
+			right++;
 		}
-		while (*rp != c && *rp != '\0')
-			rp++;
-		if (lp != rp)
+		while (*right != '\0' && *right != c)
+			right++;
+		if (left != right)
 		{
 			words++;
-			lp = rp;
+			left = right;
 		}
 	}
 	return (words);
 }
 
-static bool	set_split_str(char const *lp, char c, char **list)
+static bool	set_split_str(char const *head, char c, char **list)
 {
-	char const	*rp = lp;
+	char const	*left = head;
+	char const	*right = head;
 	size_t		i;
 
 	i = 0;
-	while (*rp != '\0')
+	while (*right != '\0')
 	{
-		while (*rp == c)
+		while (*right == c)
 		{
-			lp++;
-			rp++;
+			left++;
+			right++;
 		}
-		while (*rp != c && *rp != '\0')
-			rp++;
-		if (lp != rp)
+		while (*right != '\0' && *right != c)
+			right++;
+		if (left != right)
 		{
-			list[i] = ft_substr(lp, 0, rp - lp);
+			list[i] = ft_substr(left, 0, right - left);
 			if (list[i] == NULL)
 				return (false);
 			i++;
-			lp = rp;
+			left = right;
 		}
 	}
 	return (true);
 }
 
-static void	free_all(char **list)
+static void	*free_all(char **list)
 {
 	size_t	i;
 
@@ -62,6 +64,7 @@ static void	free_all(char **list)
 		i++;
 	}
 	free(list);
+	return (NULL);
 }
 
 char	**ft_split(char const *s, char c, size_t *list_size)
@@ -74,11 +77,8 @@ char	**ft_split(char const *s, char c, size_t *list_size)
 	list = (char **)malloc(sizeof(char *) * (*list_size + 1));
 	if (list == NULL)
 		return (NULL);
-	if (set_split_str(s, c, list))
-	{
-		list[*list_size] = NULL;
-		return (list);
-	}
-	free_all(list);
-	return (NULL);
+	if (!set_split_str(s, c, list))
+		return (free_all(list));
+	list[*list_size] = NULL;
+	return (list);
 }
