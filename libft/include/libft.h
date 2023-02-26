4ae42471b6916db976350561f1f62ca32240c8e4
habvi <a2231250601h@gmail.com>
2023-02-26 19:07:51 +0100

    update: libft

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -25,8 +25,11 @@ double	ft_min_double(const double x, const double y);
 char	**ft_split(char const *s, char c, size_t *list_size);
 char	*ft_strchr(const char *s, int int_c);
 char	*ft_strdup(const char *s);
+void    ft_strlcpy_void(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
+char    *ft_strndup(const char *s, size_t maxlen);
+size_t  ft_strnlen(const char *s, size_t maxlen);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_toupper(int c);
 
--- libft/src/libft/ft_substr.c
+++ libft/src/libft/ft_substr.c
@@ -1,34 +1,5 @@
 #include "libft.h"
 
-static size_t	ft_strnlen(const char *s, size_t maxlen)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < maxlen && s[i])
-		i++;
-	return (i);
-}
-
-static char	*ft_strndup(const char *s, size_t maxlen)
-{
-	const size_t	len_s = ft_min(maxlen, ft_strnlen(s, maxlen));
-	char			*dst;
-	size_t			i;
-
-	dst = (char *)malloc(sizeof(char) * (len_s + 1));
-	if (dst == NULL)
-		return (NULL);
-	i = 0;
-	while (i < len_s)
-	{
-		dst[i] = s[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
-}
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	len_s;
