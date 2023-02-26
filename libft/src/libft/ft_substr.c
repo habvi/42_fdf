840f88e94db1adbe7383e113e102b2b3a1d1179a
habvi <a2231250601h@gmail.com>
2023-02-26 15:34:45 +0100

    update: ft_substr faster!

--- libft/src/libft/ft_substr.c
+++ libft/src/libft/ft_substr.c
@@ -1,28 +1,42 @@
 #include "libft.h"
 
+static size_t	ft_strnlen(const char *s, size_t maxlen)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < maxlen && s[i])
+		i++;
+	return (i);
+}
+
+static char	*ft_strndup(const char *s, size_t maxlen)
+{
+	const size_t	len_s = ft_min(maxlen, ft_strnlen(s, maxlen));
+	char			*dst;
+	size_t			i;
+
+	dst = (char *)malloc(sizeof(char) * (len_s + 1));
+	if (dst == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len_s)
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	len_s;
-	size_t	tail;
-	size_t	len_substr;
-	char	*res;
 
 	if (s == NULL)
 		return (NULL);
-	len_s = ft_strlen(s);
-	if (len_s <= start || len == 0)
-	{
-		start = ft_min(len_s, start);
-		len_substr = 0;
-	}
-	else
-	{
-		tail = ft_min(len_s, start + len);
-		len_substr = tail - start;
-	}
-	res = (char *)malloc(sizeof(char) * (len_substr + 1));
-	if (res == NULL)
-		return (NULL);
-	ft_strlcpy(res, s + start, len_substr + 1);
-	return (res);
+	len_s = ft_strnlen(s, start);
+	if (len_s < start)
+		return (ft_strdup(""));
+	return (ft_strndup(s + start, len));
 }
