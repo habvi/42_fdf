1fa09fbeaf1396fc588a6745153cd22dfad876a0
habvi <a2231250601h@gmail.com>
2023-03-03 20:25:28 +0100

    update: filepath check

--- src/args.c
+++ src/args.c
@@ -5,57 +5,64 @@
 #include <stdint.h>
 
 static bool	is_correct_extension_include(\
-		const char *filepath, const size_t len_filepath, char **index)
+			const char *filepath, const size_t len_filepath, char **dot_ptr)
 {
-	*index = ft_strnstr(filepath, FILE_EXTENSION, len_filepath);
-	if (*index == NULL)
+	*dot_ptr = ft_strnstr(filepath, FILE_EXTENSION, len_filepath);
+	if (*dot_ptr == NULL)
 		return (false);
 	return (true);
 }
 
-static bool	is_valid_path(const char *filepath, char *index)
+static bool	is_valid_extension(char *dot_ptr, const size_t len_filepath)
 {
-	if (filepath == index)
-		return (false);
-	if (*(index - 1) == '/')
+	if (ft_strncmp(dot_ptr, FILE_EXTENSION, len_filepath) != 0)
 		return (false);
 	return (true);
 }
 
-static bool	is_valid_extension(const char *filepath, const size_t len_filepath)
+// ".fdf" included
+static bool	is_valid_filetype(const char *filepath, char *dot_ptr)
 {
-	while (*filepath != '.')
-		filepath++;
-	if (ft_strncmp(filepath, FILE_EXTENSION, len_filepath) != 0)
+	if (filepath == dot_ptr)
+		return (false);
+	if (*(dot_ptr - 1) == '/')
 		return (false);
 	return (true);
 }
 
-static uint8_t	check_filepath(const char *filepath)
+static bool	is_valid_filepath(const char *filepath, uint8_t *error_code)
 {
 	const size_t	len_filepath = ft_strlen(filepath);
-	char			*index;
+	char			*dot_ptr;
 
-	index = NULL;
-	if (!is_correct_extension_include(filepath, len_filepath, &index))
-		return (INVALID_EXTENSION);
-	if (!is_valid_path(filepath, index))
-		return (INVALID_FILETYPE);
-	if (!is_valid_extension(filepath, len_filepath))
-		return (INVALID_EXTENSION);
-	return (SUCCESS);
+	if (!is_correct_extension_include(filepath, len_filepath, &dot_ptr))
+	{
+		*error_code = INVALID_EXTENSION;
+		return (false);
+	}
+	if (!is_valid_extension(dot_ptr, len_filepath))
+	{
+		*error_code = INVALID_EXTENSION;
+		return (false);
+	}
+	if (!is_valid_filetype(filepath, dot_ptr))
+	{
+		*error_code = INVALID_FILETYPE;
+		return (false);
+	}
+	return (true);
 }
 
 bool	check_args(const int argc, char *const argv[], uint8_t *error_code)
 {
+	const char	*filepath = argv[1];
+
 	if (argc != 2)
 	{
-		(void)argv;
 		*error_code = INVALID_ARGC;
 		return (false);
 	}
-	*error_code = check_filepath(argv[1]);
-	if (*error_code == INVALID_EXTENSION || *error_code == INVALID_FILETYPE)
+	if (!is_valid_filepath(filepath, error_code))
 		return (false);
 	return (true);
 }
