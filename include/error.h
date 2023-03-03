cc7b6d1e0d6d81f256adfd11f2b70f3b48d9b6a2
habvi <a2231250601h@gmail.com>
2023-03-03 11:39:11 +0100

    error_code -> enum

--- include/error.h
+++ include/error.h
@@ -1,6 +1,8 @@
 #ifndef ERROR_H
 # define ERROR_H
 
+# define OPEN_ERROR					-1
+
 # define ERR_MSG_ARGS				"invalid number of arguments"
 # define ERR_MSG_FILE_EXTENSION		"invalid file extension"
 # define ERR_MSG_FILETYPE			"invalid file type"
@@ -10,16 +12,15 @@
 # define ERR_MSG_MAP_WIDTH			"invalid map width"
 # define ERR_MLX					"from mlx"
 
-# define INVALID_ARGC			    1
-# define INVALID_EXTENSION			2
-# define INVALID_FILETYPE			3
-
-# define INVALID_MAP			    4
-# define ERROR_MALLOC_FDF		    5
-# define ERROR_EMPTY_FILE		    6
-# define ERROR_MAP_WIDTH		    7
-# define ERROR_MLX  			    8
-
-# define OPEN_ERROR					-1
+typedef enum e_error_code {
+	INVALID_ARGC = 1,
+	INVALID_EXTENSION = 2,
+	INVALID_FILETYPE = 3,
+	INVALID_MAP = 4,
+	ERROR_MALLOC_FDF = 5,
+	ERROR_EMPTY_FILE = 6,
+	ERROR_MAP_WIDTH = 7,
+	ERROR_MLX = 8
+}	t_error_code;
 
 #endif
